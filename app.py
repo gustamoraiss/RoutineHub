b61bac4963bd18c41acc01fa2b0fd9140ce3e50a
Gustavo Morais <gustavombdn@gmail.com>
2026-07-09 16:11:15 -0300

    feat: altera estrutura do app.py com imports e registros de blueprints

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
+from routes.home import bp as home_bp
+from routes.estatisticas import bp as estatisticas_bp
+from routes.nova_rotina import bp as nova_rotina_bp
 
 app = Flask(__name__)
 
-@app.route('/')
-def base():
-    return render_template('base.html')
+app.register_blueprint(home_bp)
+app.register_blueprint(estatisticas_bp)
+app.register_blueprint(nova_rotina_bp)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
