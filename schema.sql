0f30cf8cc7ae8c5d7c97729da9e48bfc55c5a8a2
Gustavo Morais <gustavombdn@gmail.com>
2026-07-12 14:09:29 -0300

    feat: cria tabela de users no sql

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
