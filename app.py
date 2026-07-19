933baacc3117e08888d3866b6ef82cabc892998d
Gustavo Morais <gustavombdn@gmail.com>
2026-07-19 12:53:56 -0300

    feat: registra blueprint auth.py no app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from routes.home import bp as home_bp
 from routes.stats import bp as estatisticas_bp
 from routes.nova_rotina import bp as nova_rotina_bp
+from routes.auth import bp as auth_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(home_bp)
 app.register_blueprint(estatisticas_bp)
 app.register_blueprint(nova_rotina_bp)
+app.register_blueprint(auth_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
